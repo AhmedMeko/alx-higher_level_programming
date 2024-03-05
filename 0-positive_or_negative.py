6b424788c313fdd40cf29f61ce41a46792a00dfa
AhmedMeko <ahmmeko@gmail.com>
2024-03-05 14:50:09 -0800

    try again

--- 0-positive_or_negative.py
+++ 0-positive_or_negative.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-# YOUR CODE HERE
+
 if number > 0 :
-        print(number , 'is NEGTIVE')
+        print(number , 'is positive')
     elif number < 0 :
-            print (number , 'is positive')
+            print (number , 'is negative')
         elif number == 0:
-                print (number , 'iz ZERO')
+                print (number , 'is zero')
             else :
                     print ('ERROR')
-                        
+                    
