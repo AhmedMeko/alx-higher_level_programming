b2cee6124d13a1da7509822321e7d0c186b26d94
AhmedMeko <ahmmeko@gmail.com>
2024-03-22 11:36:19 -0700

    done

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
-/* This is Scrept is shawing me the privlege for user_0d_1 and user_0d_2 alll of this is in local host
- */
-SHOW GRANTS FOR 'user_0d_1 '@'localhost';
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/README.md
+++ 0x0E-SQL_more_queries/README.md
@@ -1,1 +1,1 @@
-Hello SQL
+More SQL
